181692872f3e18fb338f98f220358982cbc2d12e
Henry Zarza <henryacz7@gmail.com>
2021-03-26 06:30:14 -0600

    Delete product

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,44 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { any, string } from 'prop-types';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+    update,
+  });
+
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // go ahead and delete it
+          console.log('DELTEe');
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+DeleteProduct.propTypes = {
+  id: string,
+  children: any,
+};

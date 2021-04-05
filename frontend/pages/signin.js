a02304c74c59a6b27bfffe98a10e0a4036123e98
Henry Zarza <henryacz7@gmail.com>
2021-04-05 07:50:01 -0500

    Reset password

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,18 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import styled from 'styled-components';
+import SignUp from '../components/SignUp';
+import RequestReset from '../components/RequestReset';
 import Form from '../components/styles/Form';
 import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from '../components/User';
 import Error from '../components/ErrorMessage';
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 
 const SIGNIN_MUTATION = gql`
   mutation SIGNIN_MUTATION($email: String!, $password: String!) {
@@ -45,34 +54,38 @@ export default function SignIn() {
       ? data?.authenticateUserWithPassword
       : undefined;
   return (
-    <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
-      <Error error={error} />
-      <fieldset>
-        <label htmlFor="email">
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email Address"
-            autoComplete="email"
-            value={inputs.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            autoComplete="password"
-            value={inputs.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Sign In!</button>
-      </fieldset>
-    </Form>
+    <GridStyles>
+      <Form method="POST" onSubmit={handleSubmit}>
+        <h2>Sign Into Your Account</h2>
+        <Error error={error} />
+        <fieldset>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email Address"
+              autoComplete="email"
+              value={inputs.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="password"
+              value={inputs.password}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit">Sign In!</button>
+        </fieldset>
+      </Form>
+      <SignUp />
+      <RequestReset />
+    </GridStyles>
   );
 }

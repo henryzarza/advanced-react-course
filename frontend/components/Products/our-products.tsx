66e3c1301fee9c5f4dde4303bfc034b707fd9600
Henry Zarza <henryacz7@gmail.com>
2021-05-14 18:15:07 -0500

    Start our product layout

--- frontend/components/Products/our-products.tsx
+++ frontend/components/Products/our-products.tsx
@@ -1,11 +1,12 @@
-import { number } from 'prop-types';
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
+// import { useQuery } from '@apollo/client';
+// import gql from 'graphql-tag';
 import styled from 'styled-components';
-import Product from '../Product';
-import { perPage } from '../../config';
+import Product from './product';
+// import { perPage } from '../../config';
 
-export const ALL_PRODUCTS_QUERY = gql`
+export const ID_SECTION = "ourProducts";
+
+/* export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
     allProducts(first: $first, skip: $skip) {
       id
@@ -20,16 +21,34 @@ export const ALL_PRODUCTS_QUERY = gql`
       }
     }
   }
+`; */
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 79.75rem;
+  padding: 0 1rem;
+  width: 100%;
 `;
 
-const ProductsListStyles = styled.div`
+const Title = styled.h2`
+  color: var(--gray);
+  font-size: 2.5rem;
+  font-weight: 700;
+  line-height: 1.3;
+  margin-bottom: 3rem;
+  text-align: center;
+`;
+
+const List = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 60px;
+  grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+  grid-gap: 1.875rem;
 `;
 
-export default function Products({ page }) {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+export default function OurProducts() {
+  /* const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
     variables: {
       skip: page * perPage - perPage,
       first: perPage,
@@ -37,19 +56,19 @@ export default function Products({ page }) {
   });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return <p>Error: {error.message}</p>; */
 
   return (
-    <div>
-      <ProductsListStyles>
-        {data.allProducts.map((product) => (
-          <Product key={product.id} product={product} />
+    <Section id={ID_SECTION}>
+      <Title>Our Products</Title>
+      <List>
+        {[1,2,3,4,5,6,7,8].map(el => (
+          <Product key={el} />
         ))}
-      </ProductsListStyles>
-    </div>
+        {/* data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        )) */}
+      </List>
+    </Section>
   );
 }
-
-Products.propTypes = {
-  page: number,
-};
--- frontend/components/Products/product.tsx
+++ frontend/components/Products/product.tsx
@@ -1,25 +1,78 @@
-import { shape, string, number } from 'prop-types';
 import Link from 'next/link';
-import ItemStyles from './styles/ItemStyles';
-// import Title from './styles/Title';
-import PriceTag from './styles/PriceTag';
-import formatMoney from '../lib/formatMoney';
-import DeleteProduct from './DeleteProduct';
-import AddToCart from './AddToCart';
+import styled from 'styled-components';
 
-export default function Product({ product }) {
+// import ItemStyles from './styles/ItemStyles';
+// import Title from './styles/Title';
+// import PriceTag from './styles/PriceTag';
+// import formatMoney from '../lib/formatMoney';
+// import DeleteProduct from './DeleteProduct';
+// import AddToCart from './AddToCart';
+
+const Img = styled.img`
+  object-fit: cover;
+  height: 18.75rem;
+  transform: scale(1);
+  transition: transform var(--transition-duration) var(--transition-function);
+  width: 100%;
+`;
+
+const Item = styled.div`
+  background-color: var(--light-gray);
+  box-shadow: 1px 2px 4px rgba(0,0,0,0.25);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+
+  &:hover ${Img} {
+    transform: scale(1.1);
+  }
+`;
+
+const ImgContainer = styled.div`
+  overflow: hidden;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+const Name = styled.h6`
+  color: var(--gray);
+  font-size: 1.3rem;
+  font-weight: 500;
+  line-height: 1.15;
+  margin-bottom: 0.625rem;
+`;
+
+const Price = styled(Name)`
+  font-weight: 900;
+  margin-bottom: 0;
+`;
+
+export default function Product() {
   return (
-    <ItemStyles>
-      <img
-        src={product?.photo?.image?.publicUrlTransformed}
-        alt={product.name}
-      />
-      {/* <Title> */}
-      <Link href={`/product/${product.id}`}>{product.name}</Link>
-      {/* </Title> */}
-      <PriceTag>{formatMoney(product.price)}</PriceTag>
-      <p>{product.description}</p>
-      <div className="buttonList">
+    <Item>
+      <Link href="/product/3">
+        <ImgContainer>
+          <Img
+            src="https://res.cloudinary.com/dcqu0udnd/image/upload/v1621029999/sickfits/image_2_siz4xw.png"
+            alt="Mock"
+            // src={product?.photo?.image?.publicUrlTransformed}
+            // alt={product.name}
+          />
+        </ImgContainer>
+      </Link>
+      <Content>
+        <Name>Leviosa</Name>
+        <Price>$ 50.00</Price>
+      </Content>
+      {/* <Link href={`/product/${product.id}`}>{product.name}</Link> */}
+      {/* <PriceTag>{formatMoney(product.price)}</PriceTag> */}
+      {/* <div className="buttonList">
         <Link
           href={{
             pathname: 'update',
@@ -28,16 +81,16 @@ export default function Product({ product }) {
             },
           }}
         >
-          Edit ✏️
+          Edit
         </Link>
         <AddToCart id={product.id} />
         <DeleteProduct id={product.id}>Delete</DeleteProduct>
-      </div>
-    </ItemStyles>
+      </div> */}
+    </Item>
   );
 }
 
-Product.propTypes = {
+/* Product.propTypes = {
   product: shape({
     id: string,
     name: string,
@@ -50,4 +103,4 @@ Product.propTypes = {
       }),
     }),
   }),
-};
+}; */

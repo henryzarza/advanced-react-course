ab676f3bc7458fac21e8b460d6bc3b12185b3eab
Henry Zarza <henryacz7@gmail.com>
2021-05-31 19:16:45 -0500

    Move styles

--- frontend/pages/sell.tsx
+++ frontend/pages/sell.tsx
@@ -2,58 +2,13 @@ import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import { useRouter } from 'next/router';
 import { useForm } from "react-hook-form";
-import styled from 'styled-components';
 
 import Error from '../components/ErrorMessage';
 import { ALL_PRODUCTS_QUERY } from '../components/Products/our-products';
 import { FormElement } from '../components/styles/Form';
 import { MainButton } from '../components/styles/Button';
 import { SELL_FIELD_NAMES, SELL_VALIDATION_SCHEMA } from '../lib/formValidations';
-import { SM_BREAK_POINT } from '../lib/cssVariables';
-
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  min-height: 100vh;
-  padding: 0 1rem;
-  width: 100%;
-`;
-
-const Form = styled.form`
-  background-color: var(--light-gray);
-  border: 2px solid var(--white);
-  border-radius: 0.25rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), -2px -2px 4px 2px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: column;
-  max-width: 33.125rem;
-  padding: 2rem 3rem;
-  width: 100%;
-
-  @media (max-width: ${SM_BREAK_POINT}) {
-    padding: 1rem;
-  }
-`;
-
-const Title = styled.h2`
-  color: var(--gray);
-  font-weight: 600;
-  font-size: 2rem;
-  line-height: 1.2;
-  margin-bottom: 3rem;
-  position: relative;
-
-  &::after {
-    background: linear-gradient(90deg, #F52020 0%, #FAAB4D 100%);
-    bottom: -1.5rem;
-    content: '';
-    left: 0;
-    height: 0.625rem;
-    position: absolute;
-    right: 0;
-  }
-`;
+import { Container, Form, Title } from '../components/styles/BigForm';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(

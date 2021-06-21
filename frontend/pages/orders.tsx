1754937336294b209dbe773561ca4e465b6e4e9a
Henry Zarza <Henry.Zarza@walmartlabs.com>
2021-06-21 17:43:51 -0500

    Empty component

--- frontend/pages/orders.tsx
+++ frontend/pages/orders.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import Head from 'next/head';
@@ -7,6 +6,7 @@ import Link from 'next/link';
 import Error from '../components/ErrorMessage';
 import formatMoney from '../lib/formatMoney';
 import { Container, OrderItem, OrderMeta, Images } from '../components/styles/Orders';
+import Empty from '../components/Empty';
 
 const USER_ORDERS_QUERY = gql`
   query USER_ORDERS_QUERY {
@@ -37,8 +37,6 @@ function countItemsInAnOrder(order) {
   return order.items.reduce((tally, item) => tally + item.quantity, 0);
 }
 
-// const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b]);
-
 export default function OrdersPage() {
   const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
 
@@ -53,31 +51,32 @@ export default function OrdersPage() {
       <Head>
         <title>Your Orders ({allOrders.length})</title>
       </Head>
-      <h2>You have {allOrders.length} orders!</h2>
-      <Container>
-        {allOrders.map((order) => (
-          <OrderItem key={order.id}>
-            <Link href={`/order/${order.id}`}>
-              <a>
-                <OrderMeta>
-                  <span>{countItemsInAnOrder(order)} Items</span>
-                  <span className="full">
-                    {order.items.length} Product{order.items.length === 1 ? '' : 's'}
-                  </span>
-                  <span>{formatMoney(order.total)}</span>
-                </OrderMeta>
-                <Images>
-                  {order.items.map((item) => (
-                    <div key={`${order.id}-${item.id}`}>
-                      <img src={item.photo?.image?.publicUrlTransformed} alt={item.name} />
-                    </div>
-                  ))}
-                </Images>
-              </a>
-            </Link>
-          </OrderItem>
-        ))}
-      </Container>
+      {allOrders.length === 0 ? <Empty title="You don't have orders yet" /> : (
+        <Container>
+          {allOrders.map((order) => (
+            <OrderItem key={order.id}>
+              <Link href={`/order/${order.id}`}>
+                <a>
+                  <OrderMeta>
+                    <span>{countItemsInAnOrder(order)} Items</span>
+                    <span className="full">
+                      {order.items.length} Product{order.items.length === 1 ? '' : 's'}
+                    </span>
+                    <span>{formatMoney(order.total)}</span>
+                  </OrderMeta>
+                  <Images>
+                    {order.items.map((item) => (
+                      <div key={`${order.id}-${item.id}`}>
+                        <img src={item.photo?.image?.publicUrlTransformed} alt={item.name} />
+                      </div>
+                    ))}
+                  </Images>
+                </a>
+              </Link>
+            </OrderItem>
+          ))}
+        </Container>
+      )}
     </>
   );
 }
